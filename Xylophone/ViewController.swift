35a0cfe22a838b4dc6993dcdc8a8371576969eef
nasoviva <nasoviva@gmail.com>
2024-06-05 23:01:18 -0300

    add new functionality

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +18,27 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        playSound(sender: sender.titleLabel?.text ?? "C")
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
+
+    }
+
+    func playSound(sender: String) {
+        guard let path = Bundle.main.path(forResource: sender, ofType: "wav") else {
+            return
+        }
+
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch {
+            print("Error: \(error.localizedDescription)")
+        }
+    }
 }
 
