601bf509d501e4e86ee357439fefb4d312b16468
nasoviva <nasoviva@gmail.com>
2024-06-07 18:44:02 -0300

    upd code

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -12,32 +12,24 @@ class ViewController: UIViewController {
 
     var player: AVAudioPlayer?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
     @IBAction func buttonPressed(_ sender: UIButton) {
         playSound(sender: sender.titleLabel?.text ?? "C")
         sender.alpha = 0.5
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
             sender.alpha = 1.0
         }
 
     }
 
     func playSound(sender: String) {
-        guard let path = Bundle.main.path(forResource: sender, ofType: "wav") else {
-            return
-        }
-
-        let url = URL(fileURLWithPath: path)
-        do {
-            player = try AVAudioPlayer(contentsOf: url)
-            player?.play()
-        } catch {
-            print("Error: \(error.localizedDescription)")
+        if let path = Bundle.main.path(forResource: sender, ofType: "wav") {
+            let url = URL(fileURLWithPath: path)
+            do {
+                player = try AVAudioPlayer(contentsOf: url)
+                player?.play()
+            } catch {
+                print("Error: \(error.localizedDescription)")
+            }
         }
     }
 }
